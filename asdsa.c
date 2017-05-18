1eb8656756cadb9e24612110c8f3a17776eeed40
Karina Kozarova <karinakozarova1303@gmail.com>
2017-05-18 12:24:49 +0300

    Create asdsa.c

---
+++ asdsa.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+//defining a stack
+struct stack_t{
+	int *data;
+	int top;
+	int capacity;
+};
+	
+struct stack_t stack_init(int capacity){
+	sruct stack_t st;
+	st.capacity = capacity;
+	st.top = 0;
+	st.data = malloc(sizeof(int)*capacity);
+}
+
+void stack_destroy(struct stack_t* st){
+	free(st -> data);
+}
+
+int main(){
+	struct stack_t st = stack_init(5);
+
+}
