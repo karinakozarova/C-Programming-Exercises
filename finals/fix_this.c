14aed286ab9c4282df49d6fdc302ca5a474ac439
Karina Kozarova <karinakozarova1303@gmail.com>
2017-06-21 16:41:37 +0300

    Update fix_this.c

--- finals/fix_this.c
+++ finals/fix_this.c
@@ -8,21 +8,19 @@ int main(){
 	int i = 0, sum = 0;
 	array = malloc(sizeof(int)*10);	//get memory
 	while(scanf("%d",(array + i))!= EOF){	//read until EOF
-		
 		//add element to the sum
-		int element = *(array+i);
-		sum += element;
+		sum += *(array+i);
 
 		//realloc memory
 		if(i == size-1){
 			size *= 2;			
 			array = realloc(array, size);	
-					
+			
 		}
-i++;
+		i++;
 	}
-	
-	printf("%d",sum);	//print sum
 	free(array);		//free the memory
+	printf("%d\n\n",sum);	//print sum
+
 	return 0;
 }
