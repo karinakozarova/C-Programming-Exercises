837169e1a63c704b5d4aabf0baf8bef7a73e193e
karinakozarova1303@gmail.com <karinakozarova1303@gmail.com>
2017-06-23 21:03:14 +0300

    Sum rec and normal

--- Code/Reccursive/sum.c
+++ Code/Reccursive/sum.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+void sum_n_reccursive(int num,int sum,int n);
+void sum_n(int n);
 
 void sum_n_reccursive(int num,int sum,int n){
 	if(num<=n){
@@ -9,9 +11,20 @@ void sum_n_reccursive(int num,int sum,int n){
 		printf("Sum: %d\n",sum );
 	}
 }
+
+void sum_n(int n){
+	int sum = 0;
+	for(int i = 0;i<=n;i++){
+		sum += i;
+	}
+	printf("Sum: %d\n",sum);
+
+}
+
 int main()
 {
 	sum_n_reccursive(0,0,60000);	//sum 1-60000
+	sum_n(60000);	//sum 1-60000
 
 	return 0;
 }
