8ae89b4b52be51924df950add9793618bf85a8e6
Karina <karinakozarova1303@gmail.com>
2017-06-24 19:31:57 +0300

    added basic funcs

---
+++ Code/Basics/Functions/basic_functions_declarations.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// declaration of the funcs if they are after main
+//data_type name(parametres){}
+int intFunc();
+void voidFunc();
+float floatFunc();
+
+int main(int argc, char const *argv[])
+{
+	//main function, can't be compiled without it
+	return 0;
+}
+
+int intFunc(){
+	return 0; // 0 is int
+}
+
+void voidFunc(){
+	//returns nothing
+}
+
+float floatFunc(){
+	//returns float
+}
