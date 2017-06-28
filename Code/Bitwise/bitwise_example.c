bc8fa8291eb27622662b808e002e78abe25171b7
Karina <karinakozarova1303@gmail.com>
2017-06-28 14:30:25 +0300

    added bitwise operators

--- Code/Bitwise/bitwise_example.c
+++ Code/Bitwise/bitwise_example.c
@@ -9,19 +9,19 @@ int main() {
    c = a & b;       
    printf("a&b - %d\n", c );
 
-   c = a | b;       /* 61 = 0011 1101 */
+   c = a | b;      
    printf("a|b - %d\n", c );
 
-   c = a ^ b;       /* 49 = 0011 0001 */
+   c = a ^ b;       
    printf("a^b - %d\n", c );
 
-   c = ~a;          /*-61 = 1100 0011 */
+   c = ~a;      
    printf("~a - %d\n", c );
 
-   c = a << 2;     /* 240 = 1111 0000 */
+   c = a << 2;    
    printf("a << 2 - %d\n", c );
 
-   c = a >> 2;     /* 15 = 0000 1111 */
+   c = a >> 2;    
    printf("a >> 2 - %d\n", c );
 
    return 0;
