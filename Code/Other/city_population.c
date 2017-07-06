0eca3943edef6c336754505ce9aa332a89268b49
Karina Kozarova <karinakozarova1303@gmail.com>
2017-07-06 13:21:47 +0300

    Update city_population.c

--- Code/Other/city_population.c
+++ Code/Other/city_population.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int nbYear(int p0,float percent,int income,int result){
+int nbYear(int p0, double percent, int aug, int p) {
     percent /= 100;
     int count = 0;
+
     for(;;){
-        int current_result = p0 + p0 * percent + income;
-        p0 = current_result;
-        count++;
-        if(current_result == result){
-                printf("It will need %d years",count);
-                return count;
-        }
+      int current = p0+p0*percent + aug;
+      p0 = current;
+      count++;
+      if(current>=p){
+      return count;
+      }
     }
-}
+}                                               
 
 int main(){
     nbYear(1000,2,50,1200);
