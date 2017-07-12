3fb326e80622895ba3e7c6043c97ea9aa2065333
Karina <karinakozarova1303@gmail.com>
2017-07-12 22:45:26 +0300

    added sorting on exercise 6

--- Code/Entrance level/11grade/6.random_arr.c
+++ Code/Entrance level/11grade/6.random_arr.c
@@ -4,46 +4,38 @@
 
 unsigned int randr(unsigned int min, unsigned int max)
 {
-       double scaled = (double)rand()/RAND_MAX;
+   return min + rand() % (max+1 - min);
 
-       return (max - min +1)*scaled + min;
 }
 
 
 int main(){
     int x = -1;
     while(x > 10 || x < 0) scanf("%d",&x);
-    int arr[101];
+    int arr[99];
     int i = 0;
     for(;i <= 100;i++){
         arr[i] = randr(0,100);
-        printf("%d\n",arr[i]);
+    }
+    //sorting
+    int start_index = 0,finish_index = 100;
+    int sorted_arr[100];
+    for (int i = 0; i <= 100; ++i)
+    {
+      if(arr[i] % 10 == x){
+             sorted_arr[start_index] = arr[i];
+             start_index++;
+       } else if(arr[i] % 10 != x) {
+             sorted_arr[finish_index] = arr[i];
+             finish_index--;
+       }
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d\n",sorted_arr[i]);
     }
     return 0;
 }
 
-/* sorting
-	for(int x=0; x<n; x++)
 
-	{
-
-		for(int y=0; y<n-1; y++)
-
-		{
-
-			if(array[y]>array[y+1])
-
-			{
-
-				int temp = array[y+1];
-
-				array[y+1] = array[y];
-
-				array[y] = temp;
-
-			}
-
-		}
-
-	}
-	*/
